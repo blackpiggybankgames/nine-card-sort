7f6f7290a3367cc2d9546f6fa589585af9402703
blackpiggybankgames <blackpiggybankgames@users.noreply.github.com>
2026-05-16 03:16:54 +0000

    deploy: 7e86ab3ea41f3321ee759291462d6b20786c6293

--- draft/index.service.worker.js
+++ draft/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778896860|4515049';
+const CACHE_VERSION = '1778901408|4355721';
 /** @type {string} */
 const CACHE_PREFIX = 'Nine Card Sort-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
